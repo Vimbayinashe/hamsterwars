039293b1d7eb37bc05881e413026ef1bdd271aea
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-05-11 15:17:01 +0200

    Create /assets/ route

--- routes/assets.js
+++ routes/assets.js
@@ -1,14 +1,40 @@
 const { Router } = require('express');
 const router = new Router;
+const fs  = require('fs');
 
 
 router.get('/:hamster', async (req, res) => {
 
-    let hamster = req.params.hamster;
+    try {
+        
+        let hamster = req.params.hamster;
 
-    console.log(hamster);
-    
+        console.log(hamster);
+        
+        let output = ('./hamsters/' + hamster);
+        // let message = fs.existsSync(output) ? "picture found!" : "does not exist";
 
+        // 'existsSync' returns true / false
+        if (fs.existsSync(output)) {
+            console.log('The picture exists.');
+
+            res.send({ 
+                msg: `This picture ${hamster} exists`, 
+                pic: output 
+            });
+
+        } else {
+            console.log('The picture does not exist');
+            res.send({ 
+                msg: `This picture ${hamster} does NOT exist.`
+            });
+        }
+
+    }
+    catch (err) {
+        console.error(err);
+        res.status(400).send(err);
+    }
 
 
 })
