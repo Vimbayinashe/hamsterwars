20a18019c88817cb8be6a13d09c743710291dfe0
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-05-11 15:46:55 +0200

    Create firestore database

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBwOWoHB4uOpVKu0kjSb0vxdw5nzdL7hjU",
+    authDomain: "hamster-vote-app.firebaseapp.com",
+    databaseURL: "https://hamster-vote-app.firebaseio.com",
+    projectId: "hamster-vote-app",
+    storageBucket: "hamster-vote-app.appspot.com",
+    messagingSenderId: "64374202681",
+    appId: "1:64374202681:web:f16d884ef44edd7295c8e8",
+    measurementId: "G-2E1FHKLCSD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+const db = firebase.firestore();
+
